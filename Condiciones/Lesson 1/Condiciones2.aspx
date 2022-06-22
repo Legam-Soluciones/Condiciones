80d228aab6b9e679150409acfda28f76ad85a860
Luis Enrique Gamboa Castro <le.gamboa@gmail.com>
2022-06-22 10:30:40 -0600

    Agregar archivos de proyecto.

---
+++ Condiciones/Lesson 1/Condiciones2.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Condiciones2.aspx.cs" Inherits="Condiciones.Lesson_1.Condiciones2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>
+        "Hola Mundo"
+    </title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Obj_Resultado" runat="server" Text="">
+
+            </asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Condiciones/Promedio3.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Condiciones
+{//==================================== INICIO del namespace ====================================//
+	public partial class Promedio3 : System.Web.UI.Page
+	{//===================================== INICIO de la Clase =====================================//
+		protected void Page_Load(object sender, EventArgs e)
+		{//======================================= INICIO del Load =======================================//
+			double v_nota_1;
+			double v_nota_2;
+			double v_nota_3;
+			double v_pronedio;
+
+			v_nota_1 = 65;
+			v_nota_2 = 65;
+			v_nota_3 = 65;
+			v_pronedio = ((v_nota_1 + v_nota_2 + v_nota_3) / 3);
+			//===================================== INICIO del If =====================================//
+			if (v_pronedio >= 70)
+			{
+				Obj_Resultado.Text = "Aprobado";
+			}
+			else if (v_pronedio >= 60)
+			{
+				Obj_Resultado.Text = "Extraordinario";
+			}
+			else
+			{
+				Obj_Resultado.Text = "Reprobado";
+			}//===================================== FIN del If =====================================//
+		}//=======================================    FIN del Load =======================================//
+	}//=====================================    FIN de la Clase =====================================//
+}//====================================    FIN del namespace ====================================//
